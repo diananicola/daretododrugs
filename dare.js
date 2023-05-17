d6a2b804c4536997437b326c7ddcc4ec2e44eb78
diananicola <133892340+diananicola@users.noreply.github.com>
2023-05-17 17:10:04 +0200

    Functional version of the website
    -

---
+++ dare.js
@@ -0,0 +1,35 @@
+function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  function handleScroll() {
+    var fadeElements = document.querySelectorAll('.fade-in-text');
+    for (var i = 0; i < fadeElements.length; i++) {
+      if (isElementInViewport(fadeElements[i])) {
+        fadeElements[i].classList.add('fade-in');
+      } else {
+        fadeElements[i].classList.remove('fade-in');
+      }
+    }
+  }
+  
+  window.addEventListener('scroll', handleScroll);
+  window.addEventListener('resize', handleScroll);
+  handleScroll();
+
+  
+
+
+  
+  
+  
+  
+  
+  
+  
